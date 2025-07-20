a33da217d45f78de68e6b10791521eebe127685d
karbysCantCode <realgeekscorp@gmail.com>
2025-07-20 21:11:02 +1200

    YAY IT WORKS!

--- compiler.py
+++ compiler.py
@@ -117,17 +117,29 @@ class Compiler:
 
     def eatNumber() -> tuple[str, int]:
       nonlocal currentCharIndex
+      nonlocal character
+      nonlocal characterBufferLength
+      characterBufferLength -= 2
       number = ''
       currentCharacter = string[currentCharIndex]
       isDecimal = False
-      while currentCharacter.isdigit() or currentCharacter == '.':
-        if currentChar == '.':
+      isNegative = False
+      while currentCharacter.isdigit() or currentCharacter == '.' or currentCharacter == '-':
+        if currentCharacter == '.':
           if isDecimal:
-            self._logEvent("PROBABLY UNEXPECTED . IN NUMBER.", Compiler.Log.Severity.WARNING, 'Number Builder')
+            self._logEvent(f"PROBABLY UNEXPECTED '.' IN NUMBER @ LINE {line} CHAR {character}", Compiler.Log.Severity.WARNING, 'Number Builder')
           isDecimal = True
+        elif currentCharacter == '-':
+          if isNegative:
+            self._logEvent(f"PROBABLY UNEXPECTED '-' IN NUMBER @ LINE {line} CHAR {character}", Compiler.Log.Severity.WARNING, 'Number Builder')
+          isNegative = True
         number += currentCharacter
         currentCharIndex += 1
+        character += 1
+        characterBufferLength += 1
         currentCharacter = string[currentCharIndex]
+      currentCharIndex -= 1
+      character -= 1
       
       return number, isDecimal
 
@@ -145,18 +157,18 @@ class Compiler:
       '\\v' : '\v'
     }
     TYPEKEYWORDS = [
-      'int'
-      'float'
-      'void'
-      'bool'
+      'int',
+      'float',
+      'void',
+      'bool',
     ]
     MODIFIERS = [
-      'const'
-      'constexpr'
+      'const',
+      'constexpr',
     ]
     PRIMITIVELITERALS = [
-      'true'
-      'false'
+      'true',
+      'false',
     ]
     KEYWORDS = [
       'while'
@@ -169,71 +181,83 @@ class Compiler:
       'break'
     ]
     TYPEDECLKEYWORDS = [
-      'struct'
-      'class'
+      'struct',
+      'class',
     ]
     DELIMITERS = [
-      ';'
-      ','
-      '.'
-      '('
-      ')'
-      '['
-      ']'
-      '{'
-      '}'
+      ';',
+      ',',
+      '.',
+      '(',
+      ')',
+      '[',
+      ']',
+      '{',
+      '}',
     ]
     OPERATORS = [
-      '-'
-      '+'
-      '/'
-      '*'
-      '%'
+      '-',
+      '+',
+      '/',
+      '*',
+      '%',
     ]
     ASSIGNMENT = [
-      '='
-      '+='
-      '-='
-      '/='
-      '*='
+      '=',
+      '+=',
+      '-=',
+      '/=',
+      '*=',
 
     ]
     COMPARISONS = [
-      '=='
-      '!='
-      '<'
-      '>'
-      '<='
-      '>='
+      '==',
+      '!=',
+      '<',
+      '>',
+      '<=',
+      '>=',
     ]
     TWOCHARSYMBOLS = [
-      '='
-      '!'
-      '<'
-      '>'
-      '-'
-      '+'
-      '/'
-      '*'      
+      '=',
+      '!',
+      '<',
+      '>',
+      '-',
+      '+',
+      '/',
+      '*',      
     ]
 
     currentCharIndex : int = -1
     characterBuffer : str = ''
+    characterBufferLength : int = 0
     currentTokenIsString : bool = False
     currentChar : str = ''
     stringOpener : str = ''
 
     tokenArray : list[Compiler.Token] = []
 
-    def createToken(type : Compiler.Token.Type, specificType : Compiler.Token.SpecificType = Compiler.Token.SpecificType.NONE, dontClearLast : bool = False):
+    def createToken(type : Compiler.Token.Type, specificType : Compiler.Token.SpecificType = Compiler.Token.SpecificType.NONE, dontClearLast : bool = False, contentIsBuffer : bool = False):
       nonlocal characterBuffer
+      nonlocal characterBufferLength
 
-      if not dontClearLast and len(characterBuffer) != 0:
-        createToken(Compiler.Token.Type.IDENTIFIER, Compiler.Token.SpecificType.UNSUREIDENTIFIER, dontClearLast=True)
+      if not contentIsBuffer and not dontClearLast and len(characterBuffer) != 0:
+        createToken(Compiler.Token.Type.IDENTIFIER, Compiler.Token.SpecificType.UNSUREIDENTIFIER, dontClearLast=True, contentIsBuffer=True)
 
-      token = Compiler.Token(type, characterBuffer, line, character)
+      token = None
+
+      if contentIsBuffer and len(characterBuffer) != 0:
+        print(f'appended: {characterBuffer}')
+        token = Compiler.Token(type, characterBuffer, line, character-characterBufferLength)
+
+      else:
+        print(f'appended: {currentChar}')
+        token = Compiler.Token(type, currentChar, line, character-characterBufferLength)
+
       tokenArray.append(token)
       characterBuffer = ''
+      characterBufferLength = 0
 
     line = 1
     character = 0
@@ -242,6 +266,8 @@ class Compiler:
       currentCharIndex += 1
       character += 1
       currentChar = string[currentCharIndex]
+      print(f'current: {currentChar}, buffer: {characterBuffer}')
+      
 
       if currentChar == '\n':
           line += 1
@@ -252,9 +278,9 @@ class Compiler:
           currentTokenIsString = False
           stringOpener = ''
           if len(characterBuffer) != 1:
-            createToken(Compiler.Token.Type.LITERAL, Compiler.Token.SpecificType.STRINGLITERAL)
+            createToken(Compiler.Token.Type.LITERAL, Compiler.Token.SpecificType.STRINGLITERAL, contentIsBuffer=True)
           else:
-            createToken(Compiler.Token.Type.LITERAL, Compiler.Token.SpecificType.CHARLITERAL)
+            createToken(Compiler.Token.Type.LITERAL, Compiler.Token.SpecificType.CHARLITERAL, contentIsBuffer=True)
           continue
         else:
           if currentChar == '\\':
@@ -262,57 +288,26 @@ class Compiler:
             if coupledSymbol in ESCAPESEQUENCES:
               currentCharIndex += 1
               characterBuffer += ESCAPESEQUENCES[coupledSymbol]
+              characterBufferLength += 1
+              continue
+        characterBuffer += currentChar
+        characterBufferLength += 1
+        continue
 
         
       else:
         if currentChar in (' ', '\n'):
+          if currentChar == ' ':
+            characterBufferLength += 1
           continue
 
-        elif (currentChar == "'" or currentChar == '"'):
-          currentTokenIsString = True
-          stringOpener = currentChar
-          continue
-        elif currentChar in TWOCHARSYMBOLS:
-          nextCharacter = nextChar()
-          coupledSymbol = currentChar + nextCharacter
-          if nextCharacter == '=':
-            match coupledSymbol:
-              case sym if sym in COMPARISONS:
-                createToken(Compiler.Token.Type.COMPARISON)
-              case sym if sym in ASSIGNMENT:
-                createToken(Compiler.Token.Type.ASSIGMENT)
-          else:
-            match currentChar:
-              case char if char in COMPARISONS:
-                createToken(Compiler.Token.Type.COMPARISON)
-              case char if char in ASSIGNMENT:
-                createToken(Compiler.Token.Type.ASSIGMENT)
-              case char if char in OPERATORS:
-                createToken(Compiler.Token.Type.OPERATOR)
-          continue
-        
-        match currentChar:
-          case char if char in COMPARISONS:
-            createToken(Compiler.Token.Type.COMPARISON)
-            continue
-          case char if char in ASSIGNMENT:
-            createToken(Compiler.Token.Type.ASSIGMENT)
-            continue
-          case char if char in OPERATORS:
-            createToken(Compiler.Token.Type.OPERATOR)
-            continue
-
-        if currentChar.isdigit() and len(characterBuffer) == 0:
-          characterBuffer, isDecimal = eatNumber()
-          if isDecimal:
-            createToken(Compiler.Token.Type.LITERAL, Compiler.Token.SpecificType.DECIMALNUMBER)
-          else:
-            createToken(Compiler.Token.Type.LITERAL, Compiler.Token.SpecificType.NUMBER)
-          continue
-        elif currentChar == '/' and nextChar() == '/':
+        if currentChar == '/' and nextChar() == '/':
           endOfLine = string.find('\n',currentCharIndex)
+          
           if endOfLine != -1:
             currentCharIndex = endOfLine - 1
+            #line += 1
+            character = 0
             continue
           else:
             self._logEvent("NO NEWLINE AFTER SINGLE LINE COMMENT, ASSUMED END OF FILE", Compiler.Log.Severity.WARNING, 'String Tokeniser')
@@ -320,44 +315,97 @@ class Compiler:
         elif currentChar == '/' and nextChar() == '*':
           endOfMultilineComment = string.find('*/',currentCharIndex)
           if endOfMultilineComment != -1:
+            newLineCount = string[currentCharIndex:endOfMultilineComment].count('\n')
+            line += newLineCount
+            if newLineCount > 0:
+              lastNewline = string.rfind('\n', currentCharIndex, endOfMultilineComment) #if diff line
+              character = endOfMultilineComment - lastNewline + 1
+            else:
+              character = endOfMultilineComment + 1 #if sameline still
             currentCharIndex = endOfMultilineComment + 1
             continue
           else:
             self._logEvent("NO END TO MULILINE COMMENT, ASSUMED END OF FILE", Compiler.Log.Severity.WARNING, 'String Tokeniser')
             break
 
-        characterBuffer += currentChar
-        match characterBuffer:
-          case buffer if buffer in TYPEKEYWORDS:
-            createToken(Compiler.Token.Type.TYPEKEYWORD)
+        elif (currentChar.isdigit() or (currentChar == '-' and nextChar().isdigit())) and len(characterBuffer) == 0:
+          number, isDecimal = eatNumber()
+          characterBuffer = number
+
+          if isDecimal:
+            createToken(Compiler.Token.Type.LITERAL, Compiler.Token.SpecificType.DECIMALNUMBER, dontClearLast=True, contentIsBuffer=True)
+          else:
+            createToken(Compiler.Token.Type.LITERAL, Compiler.Token.SpecificType.NUMBER, dontClearLast=True, contentIsBuffer=True)
+          continue
+
+        elif (currentChar == "'" or currentChar == '"'):
+          currentTokenIsString = True
+          stringOpener = currentChar
+          createToken(Compiler.Token.Type.IDENTIFIER, Compiler.Token.SpecificType.UNSUREIDENTIFIER, dontClearLast=True, contentIsBuffer=True)
+          continue
+        elif currentChar in TWOCHARSYMBOLS:
+          nextCharacter = nextChar()
+          coupledSymbol = currentChar + nextCharacter
+          if nextCharacter == '=':
+            match coupledSymbol:
+              case coupledSymbol if coupledSymbol in COMPARISONS:
+                currentChar = coupledSymbol
+                createToken(Compiler.Token.Type.COMPARISON)
+              case coupledSymbol if coupledSymbol in ASSIGNMENT:
+                currentChar = coupledSymbol
+                createToken(Compiler.Token.Type.ASSIGMENT)
+          else:
+            match currentChar:
+              case currentChar if currentChar in COMPARISONS:
+                createToken(Compiler.Token.Type.COMPARISON)
+              case currentChar if currentChar in ASSIGNMENT:
+                createToken(Compiler.Token.Type.ASSIGMENT)
+              case currentChar if currentChar in OPERATORS:
+                createToken(Compiler.Token.Type.OPERATOR)
+          continue
+
+        match currentChar:
+          case currentChar if currentChar in COMPARISONS:
+            createToken(Compiler.Token.Type.COMPARISON)
             continue
-          case buffer if buffer in MODIFIERS:
-            createToken(Compiler.Token.Type.MODIFIER)
+          case currentChar if currentChar in ASSIGNMENT:
+            createToken(Compiler.Token.Type.ASSIGMENT)
             continue
-          case buffer if buffer in PRIMITIVELITERALS:
-            createToken(Compiler.Token.Type.PRIMITIVELITERAL)
+          case currentChar if currentChar in OPERATORS:
+            createToken(Compiler.Token.Type.OPERATOR)
             continue
-          case buffer if buffer in KEYWORDS:
-            createToken(Compiler.Token.Type.KEYWORD)
-            continue
-          case buffer if buffer in TYPEDECLKEYWORDS:
-            createToken(Compiler.Token.Type.TYPEDECLKEYWORD)
-            continue
-          case buffer if buffer in DELIMITERS:
-            if buffer == ';':
+          case currentChar if currentChar in DELIMITERS:
+            if currentChar == ';':
               createToken(Compiler.Token.Type.DELIMITER, Compiler.Token.SpecificType.SEMICOLON)
-            elif buffer == '{':
+            elif currentChar == '{':
               createToken(Compiler.Token.Type.DELIMITER, Compiler.Token.SpecificType.LCURLYBRACE)
-            elif buffer == '}':
+            elif currentChar == '}':
               createToken(Compiler.Token.Type.DELIMITER, Compiler.Token.SpecificType.RCURLYBRACE)
             else:
               createToken(Compiler.Token.Type.DELIMITER)
+            continue
+
+
+        match characterBuffer:
+          case characterBuffer if characterBuffer in TYPEKEYWORDS:
+            createToken(Compiler.Token.Type.TYPEKEYWORD, dontClearLast=True, contentIsBuffer=True)
+          case characterBuffer if characterBuffer in MODIFIERS:
+            createToken(Compiler.Token.Type.MODIFIER, dontClearLast=True, contentIsBuffer=True)
+          case characterBuffer if characterBuffer in PRIMITIVELITERALS:
+            createToken(Compiler.Token.Type.PRIMITIVELITERAL, dontClearLast=True, contentIsBuffer=True)
+          case characterBuffer if characterBuffer in KEYWORDS:
+            createToken(Compiler.Token.Type.KEYWORD, dontClearLast=True, contentIsBuffer=True)
+          case characterBuffer if characterBuffer in TYPEDECLKEYWORDS:
+            createToken(Compiler.Token.Type.TYPEDECLKEYWORD, dontClearLast=True, contentIsBuffer=True)
+      characterBuffer += currentChar
+      characterBufferLength += 1
+        
     
     return tokenArray
 
 
 compiler = Compiler()
-fileString = compiler._parseFileToString(Path('test.cpp').absolute())
+fileString = compiler._parseFileToString(Path('test2.cpp').absolute())
 tokenArray = compiler._tokeniseString(fileString)
 for token in tokenArray:
   print(token)
---
+++ test2.cpp
@@ -0,0 +1,16 @@
+int a = 2;
+//this is a comment test
+float b = 2.5;
+/*
+this
+is
+a
+multi
+like
+comment
+test
+*/int s = 491;
+//negative test
+int negativeInt = -256;
+float negativeFloat = -52.525;
+float invalidNegativeFloat = -2.5.1;
